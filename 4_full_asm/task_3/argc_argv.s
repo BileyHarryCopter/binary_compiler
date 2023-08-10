98f042ee3a524a3d58fc4fe265f6ad545f2dfc9e
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2023-08-10 15:05:21 +0300

    Changed method of iteration in the loop and added functions for printing

--- 4_full_asm/task_3/argc_argv.s
+++ 4_full_asm/task_3/argc_argv.s
@@ -4,51 +4,55 @@
 .type   _start, @function
 _start:
 
+    mov $0,        %rbx
+
 loop:
 
     # checking if arg[i] is not a NULL
-    mov 8(%rsp),   %rsi
-    or  %rsi,      %rsi
+    mov 8(%rsp, %rbx, 8),   %rsi
+    or  %rsi, %rsi
     je  exit
 
     # algorithm of finding a length of arg string
-    mov %rsi,      %rdi
-    sub	%ecx,      %ecx
-    sub	%al,       %al
+    mov %rsi, %rdi
+    sub	%ecx,             %ecx
+    sub	%al,              %al
     not	%ecx
     repne scasb    
-    not	%ecx       # now the length in ecx  
+    not	%ecx       # now the length in ecx
     dec %ecx
     mov %ecx,      %edx # put the length of string in %edx
 
     # write(stdout, argv[i], length(argv[i]))
-    mov $1,        %rdi
-    mov $1,        %eax
-    syscall
+    call my_print
 
     # write(stdout, " ", 1)
-    mov $1,        %rax
-    mov $1,        %rdi
     mov $probel,   %rsi
     mov $1,        %rdx
-    syscall
+    call my_print
 
-    pop %rsi
+    inc %rbx
 
     jmp loop
+
 
 exit:
 
     # write(stdout, "\n", 1)
-    mov $1,        %rax
-    mov $1,        %rdi
     mov $perenos,  %rsi
     mov $1,        %rdx
-    syscall
+    call my_print
 
     mov $60,   %rax
     xor %rdi,  %rdi
     syscall
+
+my_print:   #   %rax strores pointer on string, %rdi - it's length
+    mov $1,    %rax
+    mov $1,    %rdi
+    syscall
+    ret
+
 .size _start, . - _start
 
 .section .rodata
